e1330b11da0a62233d73f4c4f4350f2e0d47dd4d
Kyle Robbertze <kyle@paddatrapper.com>
2023-01-20 09:38:03 +0200

    feat: initial code commit

---
+++ pdfff/__main__.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from fillpdf import fillpdfs
+
+def run():
+    source = Path('forms')
+    fields = []
+    for f in source.glob('*.pdf'):
+        print(f'Handling file: {f}')
+        fillpdfs.print_form_fields(f)
+        fields += fillpdfs.get_form_fields(f).keys()
+    print('List of fields')
+    print(set(fields))
+
+    fillpdfs.write_fillable_pdf('forms/J294 - Death Notice.pdf', 'forms/J294 - Death Notice_filled.pdf', {'0': '9602215094081'})
