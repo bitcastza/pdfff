22573deb06adab57e3d8ed0ec61f96d4f09795a3
Vulth01 <118522512+Vulth-01@users.noreply.github.com>
2023-02-01 12:37:53 +0200

    feat: Attempt read and write to same pdf

--- pdfff/__main__.py
+++ pdfff/__main__.py
@@ -12,3 +12,14 @@ def run():
     print(set(fields))
 
     fillpdfs.write_fillable_pdf('forms/J294 - Death Notice.pdf', 'test/J294 - Death Notice_filled.pdf', {'0': '9602215094081'})
+
+
+    form_data = {'0': '9602215094081', 'field2': 'value2', 'field3': 'value3'}
+    #form_data[0]
+    with open('forms/J294 - Death Notice.pdf','rb') as pdf_file:
+        pdf_bytes = pdf_file.read()
+
+    filled_pdf_bytes = fillpdfs.write_fillable_pdf(pdf_file, 'test/J294 - Death Notice_filled.pdf', form_data[0])
+
+    with open('forms/J294 - Death Notice.pdf') as filled_pdf_file:
+        filled_pdf_file.write(filled_pdf_bytes)
