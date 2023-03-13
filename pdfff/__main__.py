900f6bc0325bf418ed5565381ca06f20a60f3a70
Kyle Robbertze <kyle@paddatrapper.com>
2023-03-13 13:03:37 +0000

    perf: lint files

--- pdfff/__main__.py
+++ pdfff/__main__.py
@@ -7,12 +7,13 @@ import yaml
 
 data = {}
 
-def getInput():        
+
+def getInput():
     print("Input Anything To Continue:")
 
     questions = [
         "surname",
-        "first name",        
+        "first name",
         "id",
         "Deceased ID",
         "Deceased Full Name",
@@ -85,7 +86,7 @@ def getInput():
 
     for question in questions:
         answers[question] = input(f"Please insert {question}: ")
-        
+
     answers["fullname"] = f"{answers['first name']} {answers['surname']}"
     answers["name and surname"] = f"{answers['first name']} {answers['surname']}"
 
@@ -95,32 +96,37 @@ def getInput():
 
 
 def run():
-    config = parse_config('pdfff/config.yml')  
+    config = parse_config("pdfff/config.yml")
     user_details = getInput()
-    
-    source = Path('forms')
+
+    source = Path("forms")
     fields = []
-    for f in source.glob('*.pdf'):
-        print(f'Handling file: {f}')
+    for f in source.glob("*.pdf"):
+        print(f"Handling file: {f}")
         fillpdfs.print_form_fields(f)
         fields += fillpdfs.get_form_fields(f).keys()
-    print('List of fields')
+    print("List of fields")
     print(set(fields))
 
-    print('List of fields \n\n\n')
+    print("List of fields \n\n\n")
 
     for config_file in config:
         output_fields = {}
-        for question_id,field in config_file["fields"].items():
+        for question_id, field in config_file["fields"].items():
             try:
                 output_fields[field] = user_details[question_id]
             except KeyError:
                 continue
-        fillpdfs.write_fillable_pdf(f"forms/{config_file['filename']}", f"test/{config_file['filename']}", output_fields, False)
+        fillpdfs.write_fillable_pdf(
+            f"forms/{config_file['filename']}",
+            f"test/{config_file['filename']}",
+            output_fields,
+            False,
+        )
 
     print("\n\n Config File: \n\n")
     print(config)
-    
+
 
 if __name__ == "__main__":
     run()
